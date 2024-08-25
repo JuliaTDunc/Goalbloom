e92e3646325e1d97f79b60cc2e1bf41e086a8f45
Julia Duncan <juliataylorduncan@gmail.com>
2024-08-24 19:27:30 -0700

    seed files created

---
+++ app/seeds/transactions.py
@@ -0,0 +1,32 @@
+from app.models import db, Transaction, environment, SCHEMA
+from sqlalchemy.sql import text
+from datetime import date
+
+def seed_transactions():
+    trans_1 = Transaction(
+        user_id=1,name="Dog Walking",amount=80,date=date(2024,9,7),frequency="two weeks",expense=False)
+    trans_2 = Transaction(
+        user_id=1,name="Car Payment",amount=240,date=date(2024,9,6),frequency="once a month",expense=True,expense_type="Transportation")
+    trans_3 = Transaction(
+        user_id=1,name="Paycheck",amount=520,date=date(2024,8,25),frequency="two weeks",expense=False)
+    trans_4 = Transaction(
+        user_id=1,name="Textbooks",amount=950,date=date(2024,10,1),frequency="once",expense=True,expense_type="Education")
+    trans_5 = Transaction(
+        user_id=1,name="Netflix",amount=14.99,date=date(2024,8,27),frequency="once a month",expense=True,expense_type="Entertainment")
+    
+
+    db.session.add(trans_1)
+    db.session.add(trans_2)
+    db.session.add(trans_3)
+    db.session.add(trans_4)
+    db.session.add(trans_5)
+    db.session.commit()
+
+
+    def undo_transactions():
+        if environment == "production":
+            db.session.excecute(f"TRUNCATE table {SCHEMA}.transactions RESTART IDENTITY CASCADE;")
+        else:
+            db.session.excecute(text("DELETE FROM transactions"))
+        
+        db.session.commit()
