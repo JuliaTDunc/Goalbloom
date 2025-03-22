6c46c8748466d442065f32b7c61cb80f848ad23c
Julia Duncan <juliataylorduncan@gmail.com>
2025-03-21 21:26:37 -0700

    file structure change

---
+++ react-vite/src/components/Budgets/BudgetsPage/BudgetsPage.jsx
@@ -0,0 +1,169 @@
+import {useEffect, useState, useMemo} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchBudgets} from '../../../redux/budget';
+import { fetchBudgetItemsByBudget } from '../../../redux/budgetItem';
+import { fetchTransactions } from '../../../redux/transaction';
+import { fetchGoals } from '../../../redux/goals';
+import { useModal } from '../../../context/Modal';
+import BudgetGraph from '../BudgetChart/BudgetChart';
+import BudgetForm from '../BudgetForm';
+//import BudgetSummary from '../BudgetSummary';
+import SavedBudgets from './SavedBudgets/SavedBudgets';
+import LoginFormModal from '../../LoginFormModal';
+import RelatedArticles from '../../ResourceLinks/RelatedArticles';
+import './BudgetsPage.css';
+
+
+
+const BudgetsPage = () => {
+    const dispatch = useDispatch();
+    const user = useSelector(state => state.session.user);
+    const allBudgets = useSelector(state => state.budgets.allBudgets);
+    const currentBudget = useSelector(state => state.budgets.currentBudget);
+    const budgetItems = useSelector(state => state.budgetItems.budgetItems);
+    const allTransactions = useSelector(state => state.transactions.allTransactions);
+    const allGoals = useSelector(state => state.goals.allGoals);
+    const [loadedSavedDetail, setLoadedSavedDetail] = useState(false);
+
+    const budgets = useMemo(() => Object.values(allBudgets), [allBudgets]);
+    const transactions = useMemo(() => Object.values(allTransactions), [allTransactions]);
+    const goals = useMemo(() => Object.values(allGoals), [allGoals]);
+    
+    const { setModalContent } = useModal();
+    const [currBudget, setCurrBudget] = useState(null);
+    const [currBudgetItems, setCurrBudgetItems] = useState([]);
+    let userData = {};
+    let summaryData;
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (user) {
+                await Promise.all([
+                    dispatch(fetchBudgets()),
+                    dispatch(fetchTransactions()),
+                    dispatch(fetchGoals()),
+                ]);
+                setLoadedSavedDetail(true);
+            }
+        };
+        fetchData();
+    }, [user, dispatch]);
+
+    // set currbudget INSTEAD of set currBUDGETITEMS
+    useEffect(() => {
+        if (currentBudget) {
+            dispatch(fetchBudgetItemsByBudget(currentBudget.id)).then(() => {
+                setCurrBudgetItems(currentBudget.id);
+            });
+        }
+    }, [currentBudget, dispatch]);
+
+    useEffect(() => {
+        if (budgets.length > 0 && !currentBudget) {
+            const today = new Date();
+            const closestBudget = budgets.reduce((closest, budget) => {
+                const budgetDate = new Date(budget.start_date);
+                return Math.abs(budgetDate - today) < Math.abs(new Date(closest.start_date) - today)
+                    ? budget
+                    : closest;
+            }, budgets[0]);
+            if (!currBudget || currBudget.id !== closestBudget.id) {
+                setCurrBudget(closestBudget);
+            }
+        }
+    }, [budgets, currentBudget, currBudget]);
+
+    // needs to include GOALS
+
+    useEffect(() => {
+        if (currentBudget && budgetItems) {
+            const filteredItems = budgetItems.filter(item => item.budget_id === currentBudget.id);
+            const transactionItems = filteredItems.filter(item => item.transaction);
+
+            const totalExpenseAmount = transactionItems
+                .map(item => transactions.find(transaction => transaction.id === item.item_id && transaction.expense))
+                .filter(transaction => transaction !== undefined)
+                .reduce((sum, transaction) => sum + transaction.amount, 0);
+          
+                if (currentBudget){
+                userData.remainingBalance = (currentBudget.total_amount - totalExpenseAmount),
+                userData.totalIncome = currentBudget.total_amount
+                };
+
+            console.log('User data: ', userData);
+
+            summaryData = {
+                ...currentBudget,
+                budgetItems: filteredItems
+            };
+        };
+    },[currentBudget])
+
+    const updateCurrentBudget = (budget) => {
+        setCurrBudget(budget);
+    };
+    const openNewBudgetModal = () => {
+        setModalContent(<BudgetForm budget={null}/>)
+    }
+
+    
+    return user? (
+        <div className='budgets-page'>
+            <div className='top-budgets-page-section'>
+                <section className='budgets-description animate__animated animate__slideInLeft'>
+                    <h2 className='feature-page-head'>Budgets</h2>
+                    <div className='feature-head-divider'></div>
+                    <p className='feature-page-subhead'>Welcome to your Budgets Page</p>
+                    {!allBudgets ? (<p className='feature-page-subhead'>To get started, select dates for your budget and simply choose the Transaction and Goal items you want to apply to your budget. </p>) :
+                        <p className='feature-page-subhead'>Check out your budgets down below</p>}
+                </section>
+
+                <div className='new-budget-button'>
+                    <button className='feature-page-create-button' onClick={() => openNewBudgetModal()}>
+                        New Budget
+                    </button>
+                </div>
+
+                <div className='budgets-content'>
+                    <div className='current-budget-section'>
+                        {currBudget ? (
+                            <div className='budget-chart'>
+                                <BudgetGraph budget={currBudget} />
+                            </div>
+                        ) : (
+                            <div className='budget-chart'>
+                                <p>Select a Budget</p>
+                            </div>
+                        )}
+                    </div>
+                    {/*<div className='current-summary-section'>
+                        {summaryData && (
+                            <div>
+                                <BudgetSummary budgetDetails={summaryData}/>
+                            </div>
+                        )}
+                    </div>*/}
+                    {loadedSavedDetail ? (
+                        <div className='saved-budgets'>
+                            <h2>Saved Budgets</h2>
+                            <div className='table-div'>
+                                <SavedBudgets
+                                    budgets={budgets}
+                                    transactions={transactions}
+                                    goals={goals}
+                                    updateCurrentBudget={updateCurrentBudget}
+                                />
+                            </div>
+                        </div>) : (<p>Loading...</p>)
+                    }
+                </div>
+            </div>
+            <div className='bottom-budgets-page-section'>
+                {/*<div className='helpful-resources'><p className='box-placeholder'>Helpful Resources</p></div>*/}
+                {userData && <div className='related-articles-budgets-page'><RelatedArticles userData={userData} /></div>}
+            </div>
+            </div>
+    ) : (setModalContent(<LoginFormModal/>))
+};
+
+export default BudgetsPage;
---
+++ react-vite/src/components/Goals/GoalsPage/GoalsPage.jsx
@@ -0,0 +1,111 @@
+import React, {useState, useMemo, useEffect} from 'react';
+import { useSelector, useDispatch} from 'react-redux';
+import {FaThLarge, FaThList} from 'react-icons/fa'
+import GoalCard from '../GoalCard';
+import { fetchGoals } from '../../../redux/goals';
+import { useModal } from '../../../context/Modal';
+import GoalForm from '../GoalForm/GoalForm';
+import LoginFormModal from '../../Auth/LoginFormModal';
+import RelatedArticles from '../../ResourceLinks/RelatedArticles';
+import './GoalsPage.css'
+
+
+
+const GoalsPage = () => {
+    const dispatch = useDispatch();
+    const user = useSelector(state => state.session.user);
+    const allGoals = useSelector(state => state.goals.allGoals);
+    const goalsArr = useMemo(() => Object.values(allGoals), [allGoals]);
+    const [listStyle, setListStyle] = useState('grid');
+    const [currGoal,setCurrGoal] = useState(null);
+    const { setModalContent } = useModal();
+    let userData;
+
+    useEffect(() => {
+        dispatch(fetchGoals());
+    }, [dispatch]);
+    
+    useEffect(() => {
+        if (user) {
+            dispatch(fetchGoals());
+        }
+    }, [dispatch, user]);
+
+    useEffect(() => {
+        if(currGoal){
+            return;
+        }else if(!currGoal && goalsArr.length){
+            console.log('yellow' ,goalsArr[goalsArr.length-1])
+            setCurrGoal(goalsArr[goalsArr.length - 1]);
+        }
+    }, [goalsArr, currGoal])
+   
+    const openNewGoalModal = () => {
+        setModalContent(<GoalForm goal={null}/>);
+    }
+    if (allGoals && goalsArr.length) {
+        const userDataTotalAmount = goalsArr
+            .reduce((sum, goal) => sum + parseFloat(goal.total_amount), 0);
+        const userDataSaved = goalsArr
+            .reduce((sum, goal) => sum + parseFloat(goal.saved_amount), 0);
+        userData = {
+            difference : (userDataTotalAmount - userDataSaved),
+            totalAmount: userDataTotalAmount
+        };
+    }
+  
+    return user? (
+        <div className="goals-page">
+            <section className='goals-description animate__animated animate__slideInLeft'>
+                <h2 className='feature-page-head'>Goals</h2>
+                <div className='feature-head-divider'></div>
+                <p className='goals-description'>Welcome to your Goals Page!</p>
+                {!allGoals ? (<p className='feature-page-subhead'>Track all your savings goals here, and update as you get closer to your goals!</p>) :
+                    <p className='feature-page-subhead'>Check out your goals down below</p>}
+            </section>
+            <section className='goals-middle-section'>
+                <div className='goal-opt-page'>
+                    <div className='goal-list-options'>
+                        <FaThLarge onClick={() => setListStyle('grid')} /> <FaThList onClick={() => setListStyle('list')} />
+                    </div>
+                    <div className='add-goal'>
+                        <button onClick={openNewGoalModal} className='feature-page-create-button'>New Goal</button>
+                    </div>
+                </div>
+                {(listStyle === 'grid' && 
+                <div className='goal-cards-grid'>
+                    {goalsArr.map(goal => (
+                        <GoalCard key={goal.id} goal={goal} onClick={() => setCurrGoal(goal)}/>
+                    ))}
+                </div>)}
+                {(currGoal && listStyle === 'list') ?
+                (<div className='goal-list'>
+                        <div className='goal-list-container'>
+                            {goalsArr
+                                .sort((a, b) => new Date(a.end_date) - new Date(b.end_date))
+                            .map((goal) => (
+                                <div
+                                    className='goal-list-item'
+                                    key={goal.id}
+                                    onClick={() => setCurrGoal(goal)}
+                                >
+                                    <p className='goal-name'>{goal.name}</p>
+                                    <div className='goal-amount'>
+                                        <p style={{ color: '#317b31' }}>{goal.saved_amount}</p><p>/</p><p style={{color:'#bf442e'}}>{goal.amount}</p>
+                                    </div>
+                                    <p className='goal-date'>{goal.end_date}</p>
+                                </div>
+                            ))}
+                        </div>
+                        <div className='highlighted-goal'>
+                            <GoalCard className='highlighted-goal-card' goal={currGoal} />
+                        </div>
+                </div>) : null}
+                
+            </section>
+            {/*<div className='helpful-resources'><p className='box-placeholder'>Helpful Resources</p></div>*/}
+           {userData && <div className='related-articles-goals'><RelatedArticles userData={userData} /></div> }
+        </div>
+    ) : ( setModalContent(<LoginFormModal/>))
+};
+export default GoalsPage;
