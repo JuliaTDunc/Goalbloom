cc5ae12ca0d4d1b8a071ccd98bb58fa51c1a524d
J Duncan <124819703+JuliaTDunc@users.noreply.github.com>
2025-04-08 21:20:49 -0700

    O auth (#119)
    * OAuth Google backend setup

--- app/config.py
+++ app/config.py
@@ -12,3 +12,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL').replace('postgres://', 'postgresql://')
     SQLALCHEMY_ECHO = True
+    GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
